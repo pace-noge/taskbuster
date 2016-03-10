91f6ddb8e5e8a55f32dcc294e844d4dbf28607e2
nasa <nasa.freaks@gmail.com>
2016-03-11 06:17:42 +0700

    settings, static files and templates

---
+++ functional_tests/test_all_users.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import unittest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.urlresolvers import reverse
+
+
+class HomeNewVisitorTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+        #self.live_server_url = "http://127.0.0.1:8000"
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_home_title(self):
+        self.browser.get(self.get_full_url('home'))
+        self.assertIn("TaskBuster", self.browser.title)
+
+    def test_h1_css(self):
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual(h1.value_of_css_property('color'), "rgba(200, 50, 255, 1)")
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
